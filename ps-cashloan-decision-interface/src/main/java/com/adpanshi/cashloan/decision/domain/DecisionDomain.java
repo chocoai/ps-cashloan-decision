21242bad499ccc382964a52766e202110100a650
zhoushanwen <zhoushanwen8502@adpanshi.com>
2018-08-01 15:46:46 +0800

    submit

---
+++ ps-cashloan-decision-interface/src/main/java/com/adpanshi/cashloan/decision/domain/DecisionDomain.java
@@ -0,0 +1,7 @@
+package com.adpanshi.cashloan.decision.domain;
+
+/**
+ * Created by zsw on 2018/8/1 0001.
+ */
+public interface DecisionDomain {
+}
---
+++ ps-cashloan-decision-service/src/main/java/com/adpanshi/cashloan/decision/domain/DecisionNativeDomain.java
@@ -0,0 +1,10 @@
+package com.adpanshi.cashloan.decision.domain;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by zsw on 2018/8/1 0001.
+ */
+@Service("decisionDomain")
+public class DecisionNativeDomain implements DecisionDomain {
+}
